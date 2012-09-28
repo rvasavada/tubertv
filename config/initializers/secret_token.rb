9bb4ad406d38082b388deff6da7c3a870f468bc1
Rhut Vasavada <rhutvasavada@Rhut-Vasavadas-MacBook-Pro.local>
2012-09-27 22:26:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YoutubeTv::Application.config.secret_token = '793f7c019117a6e693e8d68b38003c3313b7654d921186dd792c07f1a2a56ebe3fd8616362f21d35f141324e184cc5f7a8eaa35cc4a171393174f3054a6a5320'
