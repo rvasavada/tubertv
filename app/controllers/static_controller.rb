4151d10c2b252d40497b1930633d7596502c7630
Rhut Vasavada <rhutvasavada@Rhut-Vasavadas-MacBook-Pro.local>
2012-09-28 03:57:48 -0700

    finished v1

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,13 +1,31 @@
 class StaticController < ApplicationController
   def home
     client = YouTubeIt::Client.new
-    @client = client.videos_by(:query => "penguin")
+    @client = client.videos_by(:recently_featured, :restriction => "US")
+
   end
   
   def next_video
-    #current_user.histories.create!(:video_id => params[:video_id])      
-    client = YouTubeIt::Client.new
-    @client = client.videos_by(:query => "penguin")
-    render :json=> @client.to_json
+    if user_signed_in?
+      current_user.histories.create!(:video_id => params[:v])
+      
+      watched = current_user.histories.select(:video_id).map(&:video_id)
+      
+      client = YouTubeIt::Client.new
+      @client = client.videos_by(:recently_featured, :restriction => "US")
+      
+      next_video = ""
+      @client.videos.each do |video|
+        if !watched.include?(video.unique_id)
+          next_video = video
+          break
+        end
+      end
+      
+      render :json=> next_video.to_json
+      
+    else
+      render root_url   
+    end
   end
 end
