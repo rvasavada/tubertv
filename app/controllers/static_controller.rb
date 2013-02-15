20c18c100d6de3043f587390321a1c60094be0d2
Rhut Vasavada <rhutvasavada@Rhut-Vasavadas-MacBook-Pro.local>
2013-02-15 01:48:49 -0800

    most popular today

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,24 +7,23 @@ class StaticController < ApplicationController
     client = YouTubeIt::Client.new(:dev_key => "AI39si6oYP4GfE_J0LDuUTwWjA-4CNd1HbantYLBPMNQ7TiSz5jKdnuRC7WtOxahsdLq9JUJWvcpooNB1qxKvvYTWXt9th6dvg")
     
     #get params that have value 1
-    
     # # of vids per category = divide 180 / # of params that have value 1
     
     #for loop through each category
     
     
     #for loop through category to get to # (subtracting 25)
-     news = Crack::XML.parse(open("https://gdata.youtube.com/feeds/api/standardfeeds/US/most_popular_#{category}?time=today&start-index=1&max-results=10"))
+    #news = Crack::XML.parse(open("https://gdata.youtube.com/feeds/api/standardfeeds/US/most_popular_#{category}?time=today&start-index=#{index}&max-results=25"))
  
      
-    news["feed"]["entry"].each do |video|
-      id = video["id"].match(/videos\/(.+)/)
-      !unique_ids.include?(id[1]) ? unique_ids.push(id[1]) : 1
-    end 
+    #news["feed"]["entry"].each do |video|
+    #  id = video["id"].match(/videos\/(.+)/)
+    #  !unique_ids.include?(id[1]) ? unique_ids.push(id[1]) : 1
+    #end 
     
-    pre_playlist = unique_ids.uniq.sort_by { rand }
-    @first_vid = pre_playlist.pop
-    @playlist = pre_playlist.join(",")
+    #pre_playlist = unique_ids.uniq.sort_by { rand }
+    #@first_vid = pre_playlist.pop
+    #@playlist = pre_playlist.join(",")
 
   end
   
