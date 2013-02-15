cc88dc81fe97f919f5623196be7aaeffd072b5ef
Rhut Vasavada <rhutvasavada@Rhut-Vasavadas-MacBook-Pro.local>
2013-02-15 02:43:53 -0800

    most popular today

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -7,14 +7,10 @@ class StaticController < ApplicationController
     client = YouTubeIt::Client.new(:dev_key => "AI39si6oYP4GfE_J0LDuUTwWjA-4CNd1HbantYLBPMNQ7TiSz5jKdnuRC7WtOxahsdLq9JUJWvcpooNB1qxKvvYTWXt9th6dvg")
     @categories = []
     #get params that have value 1
-    params.each {|key, value|  @categories.push(key) if value == 1 }
-    
+    params.each do |key, value|  
+      @categories.push(key) if value == 1 } 
+    end    
 
-
-    end
-    
-
-    
     pre_playlist = unique_ids.uniq.sort_by { rand }
     @first_vid = pre_playlist.pop
     @playlist = pre_playlist.join(",")
